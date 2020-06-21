2862599a2d4d5a67bd7203f6eeb8154798c06a55
Tristan816la <TristanQue2000@outlook.com>
2020-06-20 19:41:34 -0700

    finished proj2b

---
+++ project/proj2ab/bearmaps/KDTree.java
@@ -0,0 +1,106 @@
+package bearmaps;
+
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class KDTree implements PointSet {
+    private class Node {
+        Point val;
+        int depth;
+
+        Node(Point p, Node l, Node r, int d) {
+            val = p;
+            left = l;
+            right = r;
+            depth = d;
+        }
+
+        Node left;
+        Node right;
+    }
+
+    private Node root;
+
+    public KDTree(List<Point> points) {
+        for (Point p : points) {
+            root = insert(p, root, 0);
+        }
+    }
+
+    private Node insert(Point point, Node node, int depth) {
+        if (node == null)
+            return new Node(point, null, null, depth);
+        if (node.val.equals(point)) {
+            return node; // Duplicate node
+        } else if (downOrLeft(point, node.val, depth)) {
+            node.left = insert(point, node.left, depth + 1);
+        } else {
+            node.right = insert(point, node.right, depth + 1);
+        }
+        return node;
+    }
+
+    @Override
+    public Point nearest(double x, double y) {
+        Point target = new Point(x, y);
+        return nearest(root, target, root.val);
+    }
+
+    private Point nearest(Node node, Point target, Point best) {
+        if (node == null) {
+            return best;
+        }
+        double bestDis = Point.distance(best, target);
+        double curDis = Point.distance(target, node.val);
+        if (Double.compare(bestDis, curDis) > 0) {
+            best = node.val;
+        }
+        Node goodSide;
+        Node badeSide;
+        boolean next = downOrLeft(target, node.val, node.depth);
+        if (next) { // is downer or lefter
+            goodSide = node.left;
+            badeSide = node.right;
+        } else {
+            goodSide = node.right;
+            badeSide = node.left;
+        }
+        best = nearest(goodSide, target, best);
+        if (worthChecking(node, target, best)) {
+            best = nearest(badeSide, target, best);
+        }
+        return best;
+    }
+
+    private boolean worthChecking(Node node, Point target, Point best) {
+        double bestDis = Point.distance(best, target);
+        if (node.depth % 2 == 0) {
+            return node.val.getX() - target.getX() < bestDis;
+        } else {
+            return node.val.getY() - target.getY() < bestDis;
+        }
+    }
+
+    // Return true if a is at upper or left position of b
+    private boolean downOrLeft(Point a, Point b, int depth) {
+        if (depth % 2 == 0) {
+            return Double.compare(a.getX(), b.getX()) < 0;
+        } else {
+            return Double.compare(a.getY(), b.getY()) < 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Point p1 = new Point(2, 3); // constructs a Point with x = 1.1, y = 2.2
+        Point p2 = new Point(4, 2);
+        Point p3 = new Point(4, 2);
+        Point p4 = new Point(4, 5);
+        Point p5 = new Point(3, 3);
+        Point p6 = new Point(1, 5);
+        Point p7 = new Point(4, 4);
+        Point t = new Point(0, 7);
+        KDTree nn = new KDTree(List.of(p1, p2, p3, p4, p5, p6, p7));
+        assertTrue(nn.nearest(t.getX(), t.getY()).equals(p6));
+    }
+}
---
+++ project/proj2ab/bearmaps/NaivePointSet.java
@@ -0,0 +1,34 @@
+package bearmaps;
+
+import java.util.List;
+
+public class NaivePointSet implements PointSet {
+    List<Point> allPoints;
+
+    public NaivePointSet(List<Point> points) {
+        allPoints = points;
+    }
+
+    @Override
+    public Point nearest(double x, double y) {
+        Point target = new Point(x, y);
+        Point result = allPoints.get(0);
+        double shortest = Point.distance(target, result);
+        for (Point p : allPoints) {
+            if (Point.distance(p, target) < shortest) {
+                result = p;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1.1, 2.2); // constructs a Point with x = 1.1, y = 2.2
+        Point p2 = new Point(3.3, 4.4);
+        Point p3 = new Point(-2.9, 4.2);
+
+        NaivePointSet nn = new NaivePointSet(List.of(p1, p2, p3));
+        Point ret = nn.nearest(3.0, 4.0); // returns p2
+        System.out.println(ret.getX() + " and " + ret.getY()); // evaluates to 3.3 and 4.4;
+    }
+}
